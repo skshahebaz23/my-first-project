b4f1baa00bc24c661ca08e5bfb5bc9b9325e08bf
skshahebaz23 <skshahebaz70679@gmail.com>
2025-11-24 21:57:17 +0530

    Update moti.js

--- moti.js
+++ moti.js
@@ -1,4 +1,4 @@
-let highestZ = 10;
+let highestZ = 1;
 
 class Paper {
   holding = false;
@@ -6,7 +6,7 @@ class Paper {
   startY = 0;
   paperX = 0;
   paperY = 0;
-  rotation = Math.random() * 20 - 10;
+  rotation = Math.random() * 30 - 15;
 
   getPos(e) {
     if (e.touches) {
@@ -18,68 +18,4 @@ class Paper {
     return { x: e.clientX, y: e.clientY };
   }
 
-  updateTransform(paper) {
-    paper.style.transform =
-      `translate(${this.paperX}px, ${this.paperY}px) rotate(${this.rotation}deg)`;
-  }
-
-  init(paper) {
-
-    // SAB PAPERS KO CENTER ME START KARNE KA CODE
-    const centerX = window.innerWidth / 2 - paper.offsetWidth / 2;
-    const centerY = window.innerHeight / 2 - paper.offsetHeight / 2;
-
-    this.paperX = centerX;
-    this.paperY = centerY;
-
-    paper.style.position = "fixed";
-    this.updateTransform(paper);
-
-    const move = (e) => {
-      if (!this.holding) return;
-
-      const pos = this.getPos(e);
-
-      const dx = pos.x - this.startX;
-      const dy = pos.y - this.startY;
-
-      this.paperX += dx;
-      this.paperY += dy;
-
-      this.startX = pos.x;
-      this.startY = pos.y;
-
-      this.updateTransform(paper);
-      e.preventDefault();
-    };
-
-    const down = (e) => {
-      const pos = this.getPos(e);
-
-      this.holding = true;
-      this.startX = pos.x;
-      this.startY = pos.y;
-
-      paper.style.zIndex = highestZ++;
-      e.preventDefault();
-    };
-
-    const up = () => {
-      this.holding = false;
-    };
-
-    // Mouse Events
-    paper.addEventListener("mousedown", down);
-    document.addEventListener("mousemove", move);
-    document.addEventListener("mouseup", up);
-
-    // Touch Events
-    paper.addEventListener("touchstart", down, { passive: false });
-    document.addEventListener("touchmove", move, { passive: false });
-    document.addEventListener("touchend", up);
-  }
-}
-
-document.querySelectorAll(".paper").forEach((paper) => {
-  new Paper().init(paper);
-});
+  updateTransform(paper
