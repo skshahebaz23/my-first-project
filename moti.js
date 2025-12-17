24053ab054ced2ae4ba118c8239884b8b33a8ce6
skshahebaz23 <skshahebaz70679@gmail.com>
2025-12-17 09:10:57 +0530

    Add files via upload

---
+++ moti.js
@@ -0,0 +1,95 @@
+let highestZ = 1;
+
+/* ================= MUSIC PLAY ON FIRST CLICK ================= */
+document.addEventListener("click", function () {
+  const music = document.getElementById("bgm");
+  if (music && music.paused) {
+    music.play().catch(() => {});
+  }
+}, { once: true });
+
+/* ================= PAPER CLASS ================= */
+class Paper {
+  holdingPaper = false;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+  rotating = false;
+
+  init(paper) {
+    document.addEventListener('mousemove', (e) => {
+      if (!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
+
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY) || 1;
+
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
+      if (this.holdingPaper) {
+        if (!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        paper.style.transform =
+          `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    });
+
+    paper.addEventListener('mousedown', (e) => {
+      if (this.holdingPaper) return;
+
+      this.holdingPaper = true;
+      paper.style.zIndex = highestZ++;
+      
+      this.mouseTouchX = this.mouseX;
+      this.mouseTouchY = this.mouseY;
+      this.prevMouseX = this.mouseX;
+      this.prevMouseY = this.mouseY;
+
+      if (e.button === 2) {
+        this.rotating = true;
+      }
+    });
+
+    window.addEventListener('mouseup', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
+  }
+}
+
+/* ================= INIT PAPERS ================= */
+const papers = Array.from(document.querySelectorAll('.paper'));
+
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
+});
